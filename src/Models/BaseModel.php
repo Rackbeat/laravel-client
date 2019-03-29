599b87830455ea9ed4ba6ca8b570b94002a22a55
Lasse Rafn <lasserafn@gmail.com>
2019-03-29 11:35:14 +0100

    Prep models

--- src/Models/BaseModel.php
+++ src/Models/BaseModel.php
@@ -8,9 +8,13 @@ class BaseModel
 	protected $data     = [];
 	protected $original = [];
 
+	/**
+	 * BaseModel constructor.
+	 *
+	 * @param array|object $data
+	 */
 	public function __construct( $data = [] ) {
-		$this->data     = $data;
-		$this->original = $data;
+		$this->setData( $data );
 	}
 
 	public function __get( $name ) {
@@ -19,7 +23,7 @@ class BaseModel
 
 	public function __set( $name, $value ) {
 		if ( \in_array( $name, [ 'data', 'original' ] ) ) {
-			$this[ $name ] = $value;
+			$this->{$name} = $value;
 
 			return;
 		}
@@ -34,4 +38,37 @@ class BaseModel
 	public function toArray() { }
 
 	public function toJson() { }
+
+	public function toObject() { }
+
+	public function getData() {
+		return $this->data;
+	}
+
+	public function getOriginal() {
+		return $this->original;
+	}
+
+	public function getDirty() {
+		return array_filter( $this->original, function ( $value, $key ) {
+			return $this->data[ $key ] !== $value;
+		}, ARRAY_FILTER_USE_BOTH );
+	}
+
+	public function refresh() {
+		// todo implement method to GET fresh data
+	}
+
+	/**
+	 * @param array $data
+	 */
+	protected function setData( $data = [] ) {
+		if ( \is_object( $data ) ) {
+			// todo find a faster way to do this
+			$data = json_decode( json_encode( $data ), true );
+		}
+
+		$this->data     = $data;
+		$this->original = $data;
+	}
 }
--- tests/Models/ModelGetterSetterTest.php
+++ tests/Models/ModelGetterSetterTest.php
@@ -5,12 +5,42 @@ use PHPUnit\Framework\TestCase;
 class ModelGetterSetterTest extends TestCase
 {
 	/** @test */
-	public function strips_namespaces_by_default() {
-		$reader = new \Rackbeat\XmlReader( [ 'attribute_prefix' => 'h' ] );
+	public function can_set_data_fields_in_constructor_with_an_array() {
+		$model = new \Rackbeat\Models\BaseModel( [
+			'id'   => 1,
+			'name' => 'John Doe',
+		] );
 
-		$this->assertArrayHasKey( 'root', $reader->fromPath( __DIR__ . '/examples/namespaces.xml' ) );
-		$this->assertArrayHasKey( 'table', $reader->fromPath( __DIR__ . '/examples/namespaces.xml' )['root'] );
-		$this->assertArrayHasKey( 'tr', $reader->fromPath( __DIR__ . '/examples/namespaces.xml' )['root']['table'] );
-		$this->assertArrayHasKey( 'td', $reader->fromPath( __DIR__ . '/examples/namespaces.xml' )['root']['table']['tr'] );
+		var_dump( $model->getData() );
+		$this->assertTrue( true );
 	}
+
+	/** @test */
+	public function can_set_data_fields_in_constructor_with_an_json_object() {
+		$model = new \Rackbeat\Models\BaseModel( json_decode( json_encode( [
+			'id'   => 1,
+			'name' => 'John Doe',
+		] ) ) );
+
+		var_dump( $model->id );
+		var_dump( $model->getData() );
+		$this->assertTrue( true );
+	}
+
+	/** @test */
+	public function can_override_data_directly_with_an_array() {
+		$model       = new \Rackbeat\Models\BaseModel();
+		$model->data = [ 'a' => 'f' ];
+
+		var_dump( $model->getData() );
+	}
+
+	/** @test */
+	public function can_override_data_directly_with_an_object() { }
+
+	/** @test */
+	public function cannot_override_data_directly_with_a_non_array_or_object_value() { }
+
+	/** @test */
+	public function cannot_override_original_values() { }
 }
