8873cb65d9086eb0dda70424c6274570f361a647
Lasse Rafn <lasserafn@gmail.com>
2020-10-09 15:00:14 +0100

    Fix item resource

--- src/Resources/ItemResource.php
+++ src/Resources/ItemResource.php
@@ -3,9 +3,13 @@
 namespace RackbeatSDK\Resources;
 
 use RackbeatSDK\Models\Item;
+use RackbeatSDK\Resources\Traits\CanFind;
+use RackbeatSDK\Resources\Traits\CanIndex;
 
-class ItemResource extends CrudResource
+class ItemResource extends BaseResource
 {
+	use CanIndex, CanFind;
+
 	protected const MODEL         = Item::class;
 	protected const RESOURCE_KEY  = 'item';
 	protected const ENDPOINT_BASE = 'items';
