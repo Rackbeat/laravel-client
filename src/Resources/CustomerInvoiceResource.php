05de5d5652207d8c0927ddcb6bc18e77dad56c3d
Lasse Rafn <lasserafn@gmail.com>
2020-10-25 14:25:50 +0000

    Fix

--- src/Resources/CustomerInvoiceResource.php
+++ src/Resources/CustomerInvoiceResource.php
@@ -8,5 +8,5 @@ class CustomerInvoiceResource extends CrudResource
 {
 	protected const MODEL         = CustomerInvoice::class;
 	protected const RESOURCE_KEY  = 'customer_invoice';
-	protected const ENDPOINT_BASE = 'customer_invoices';
+	protected const ENDPOINT_BASE = 'customer-invoices';
 }
