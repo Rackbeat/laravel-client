61d20f0e2e9c7b2109a9f0472ab7d5c126f9b6da
Lasse Rafn <lasserafn@gmail.com>
2020-10-25 14:34:27 +0000

    Get pdf

--- src/Resources/CustomerInvoiceResource.php
+++ src/Resources/CustomerInvoiceResource.php
@@ -2,6 +2,7 @@
 
 namespace RackbeatSDK\Resources;
 
+use RackbeatSDK\API;
 use RackbeatSDK\Models\CustomerInvoice;
 
 class CustomerInvoiceResource extends CrudResource
@@ -9,4 +10,8 @@ class CustomerInvoiceResource extends CrudResource
 	protected const MODEL         = CustomerInvoice::class;
 	protected const RESOURCE_KEY  = 'customer_invoice';
 	protected const ENDPOINT_BASE = 'customer-invoices';
+
+	public function getPdf(int $invoiceNumber) {
+		return API::http()->get(self::ENDPOINT_BASE . '/' . $invoiceNumber . '.pdf');
+	}
 }
