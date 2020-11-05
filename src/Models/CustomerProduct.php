5fa5cbcf57125e141734d6accd5db694ed3fad5b
Lasse Rafn <lasserafn@gmail.com>
2020-11-05 10:23:25 +0000

    Prep

--- src/Models/CustomerProduct.php
+++ src/Models/CustomerProduct.php
@@ -3,15 +3,17 @@
 namespace RackbeatSDK\Models;
 
 /**
- * @property string       $name
- * @property string       $type
- * @property string       $number
- * @property string       $urlfriendly_number
- * @property float|double $sales_price
- * @property float|double $regular_sales_price
- * @property boolean      $is_custom_sales_price
- * @property float|double $discount_percentage
- * @property string       $price_source
+ * @property string            $name
+ * @property string            $type
+ * @property string            $number
+ * @property string            $urlfriendly_number
+ * @property float|double      $sales_price
+ * @property float|double      $regular_sales_price
+ * @property boolean           $is_custom_sales_price
+ * @property float|double      $discount_percentage
+ * @property string            $price_source
+ *
+ * @property null|ProductGroup $group (optionally returned)
  */
 class CustomerProduct extends Model
 {
@@ -25,5 +27,15 @@ class CustomerProduct extends Model
 		'is_custom_sales_price' => 'boolean',
 		'discount_percentage'   => 'double',
 		'price_source'          => 'string',
+
+		'group' => ProductGroup::class,
 	];
+
+	public function getMetadataAttribute( array $value )
+	{
+		return array_map( function ( $metaObject ) {
+			return (object) $metaObject;
+		}, $value );
+	}
+
 }
