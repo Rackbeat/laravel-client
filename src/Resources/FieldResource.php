a1920747d171ec519d9e76329e4734d52903b548
Lasse Rafn <lasserafn@gmail.com>
2020-11-13 10:34:57 +0000

    Helpers

--- src/Resources/FieldResource.php
+++ src/Resources/FieldResource.php
@@ -9,4 +9,25 @@ class FieldResource extends CrudResource
 	protected const MODEL         = Field::class;
 	protected const RESOURCE_KEY  = 'field';
 	protected const ENDPOINT_BASE = 'fields';
+
+	public function forCustomers(): FieldResource
+	{
+		$this->where( 'available_for', 'customer' );
+
+		return $this;
+	}
+
+	public function forSuppliers(): FieldResource
+	{
+		$this->where( 'available_for', 'supplier' );
+
+		return $this;
+	}
+
+	public function forItems(): FieldResource
+	{
+		$this->where( 'available_for', 'item' );
+
+		return $this;
+	}
 }
