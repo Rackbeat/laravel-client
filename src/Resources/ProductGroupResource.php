0c754de460533bac5c16997ba8f1347e8e4ec682
Lasse Rafn <lasserafn@gmail.com>
2020-11-26 15:34:35 +0000

    Fix

--- src/Resources/ProductGroupResource.php
+++ src/Resources/ProductGroupResource.php
@@ -8,5 +8,5 @@ class ProductGroupResource extends CrudResource
 {
 	protected const MODEL         = ProductGroup::class;
 	protected const RESOURCE_KEY  = 'product_group';
-	protected const ENDPOINT_BASE = 'product_groups';
+	protected const ENDPOINT_BASE = 'product-groups';
 }
