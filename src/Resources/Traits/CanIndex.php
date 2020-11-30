06f55c574deb950842a6faba59a1415f0d41df0d
Lasse Rafn <lasserafn@gmail.com>
2020-11-30 11:29:50 +0000

    Can check if exists

--- src/Resources/Traits/CanIndex.php
+++ src/Resources/Traits/CanIndex.php
@@ -10,6 +10,7 @@ use RackbeatSDK\Models\Model;
  * @method PaginatedIndexResponse|IndexResponse get( $page = 1, $perPage = 20, array $query = [] )
  * @method IndexResponse all( array $query = [] )
  * @method Model first( array $query = [] )
+ * @method boolean exists()
  */
 trait CanIndex
 {
